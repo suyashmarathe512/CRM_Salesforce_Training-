4bc4e9c55625d628ac56f1dd1821e4ca9128cb92
Suyash <suyashmarathe1092003@gmail.com>
2025-09-19 11:51:10 +0530

    Problem 1 of the VisualForce Assignment

--- force-app/main/default/staticresources/DeleteAccountScript/DeleteAccountScript.js
+++ force-app/main/default/staticresources/DeleteAccountScript/DeleteAccountScript.js
@@ -1,18 +1,18 @@
-<apex:page controller="AccountsInformationController" >
-<script>
-    function deleteAccount(accountId) {
-        Visualforce.remoting.Manager.invokeAction(
-            '{!$RemoteAction.DeleteAccountScriptController.deleteAccount}',
-            accountId,
-            function(result, event) {
-                if (event.status) {
-                    alert('The account has been deleted. Reload the page to update the list.');
-                } else {
-                    alert('Failed to delete account: ' + event.message);
-                }
-            },
-            {escape: true}
-        );
-    }
-</script>
-</apex:page>
+function deleteAccount(accountId) {
+    console.log('deleteAccount called with accountId: ' + accountId);
+    alert('deleteAccount called with accountId: ' + accountId);
+    Visualforce.remoting.Manager.invokeAction(
+        '{!$RemoteAction.AccountsInformationController.deleteAccountRemote}',
+        accountId,
+        function(result, event) {
+            console.log('result: ' + result + ', event.status: ' + event.status);
+            if(event.status) {
+                alert(result);
+                location.reload();
+            } else {
+                alert('Error: ' + result);
+            }
+        },
+        {escape: true}
+    );
+}

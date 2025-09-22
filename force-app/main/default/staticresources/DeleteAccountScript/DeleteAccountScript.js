5b9207c610782e2c6fa02fb40f32071b364ccba2
Suyash <suyashmarathe1092003@gmail.com>
2025-09-22 14:26:54 +0530

    Problem 1 of the VisualForce Pages

--- force-app/main/default/staticresources/DeleteAccountScript/DeleteAccountScript.js
+++ force-app/main/default/staticresources/DeleteAccountScript/DeleteAccountScript.js
@@ -2,7 +2,7 @@ function deleteAccount(accountId) {
     console.log('deleteAccount called with accountId: ' + accountId);
     alert('deleteAccount called with accountId: ' + accountId);
     Visualforce.remoting.Manager.invokeAction(
-        '{!$RemoteAction.AccountsInformationController.deleteAccountRemote}',
+        'AccountsInformationController.deleteAccountRemote',
         accountId,
         function(result, event) {
             console.log('result: ' + result + ', event.status: ' + event.status);

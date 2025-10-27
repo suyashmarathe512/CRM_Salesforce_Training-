9a54310a48685db5b5409e08b3bacb2203b03537
Suyash <suyashmarathe1092003@gmail.com>
2025-10-27 15:27:10 +0530

    Q1) Calculator UI

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -4,27 +4,21 @@ export default class CalculatorInLwc extends LightningElement {
     @track secondNumber;
     @track resultValue;
     @track errorMessage = '';
-    // Handle change for first number input
     handleNumberOneChange(event) {
         this.firstNumber = Number(event.target.value) || 0;
     }
-    // Handle change for second number input
     handleNumberTwoChange(event) {
         this.secondNumber = Number(event.target.value) || 0;
     }
-    // Perform addition
     addition() {
         this.resultValue = this.firstNumber + this.secondNumber;
     }
-    // Perform multiplication
     multiplication() { 
         this.resultValue = this.firstNumber * this.secondNumber;
     }
-    // Perform subtraction
     subtraction() {
         this.resultValue = this.firstNumber - this.secondNumber;
     }
-    // Perform division
     division() {
         if (this.secondNumber == 0) {
             this.resultValue = 'Cannot divide by zero'; 

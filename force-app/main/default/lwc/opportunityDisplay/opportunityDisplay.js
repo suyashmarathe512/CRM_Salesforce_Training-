9d4e251c3de5b9813fc59b581b2a5bf10f01d3e9
Suyash <suyashmarathe1092003@gmail.com>
2025-10-27 17:31:05 +0530

    Q4) Opportunity Record Using LDS

--- force-app/main/default/lwc/opportunityDisplay/opportunityDisplay.js
+++ force-app/main/default/lwc/opportunityDisplay/opportunityDisplay.js
@@ -4,14 +4,11 @@ import OPPORTUNITY_NAME from '@salesforce/schema/Opportunity.Name';
 import OPPORTUNITY_STAGE_NAME from '@salesforce/schema/Opportunity.StageName';
 import OPPORTUNITY_AMOUNT from '@salesforce/schema/Opportunity.Amount';
 import OPPORTUNITY_CLOSE_DATE from '@salesforce/schema/Opportunity.CloseDate';
-
 const FIELDS = [OPPORTUNITY_NAME, OPPORTUNITY_STAGE_NAME, OPPORTUNITY_AMOUNT, OPPORTUNITY_CLOSE_DATE];
-
 export default class OpportunityDisplay extends LightningElement {
     @api recordId;
     opportunity;
     error;
-
     @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
     wiredOpportunity({ error, data }) {
         if (data) {
@@ -22,19 +19,15 @@ export default class OpportunityDisplay extends LightningElement {
             this.opportunity = undefined;
         }
     }
-
     get name() {
         return this.opportunity.fields.Name.value;
     }
-
     get stageName() {
         return this.opportunity.fields.StageName.value;
     }
-
     get amount() {
         return this.opportunity.fields.Amount.value;
     }
-
     get closeDate() {
         return this.opportunity.fields.CloseDate.value;
     }
